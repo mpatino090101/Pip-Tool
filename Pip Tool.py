7e1e661d744d22bff8d6ca89df498f37cc15e549
mpatino090101 <51469147+mpatino090101@users.noreply.github.com>
2022-05-31 16:52:45 -0400

    Add files via upload

---
+++ Pip Tool.py
@@ -0,0 +1,38 @@
+import os
+
+def install_module():
+    instruction = "pip install " + input("What module would you like to install?")
+    os.system(instruction)
+    print("Congratulations, your module has been successfully installed!")
+
+def uninstall_module():
+    instruction = "pip uninstall " + input("What module would you like to uninstall?")
+    os.system(instruction)
+    print("Congratulations, your module has been successfully uninstalled!")
+
+def check_version():
+    print(os.system("pip --version"))
+
+def upgrade_pip_version():
+    print(os.system("pip install --upgrade pip"))
+
+def command_choice(choice):
+    match choice:
+        case 1:
+            install_module()
+        case 2:
+            uninstall_module()
+        case 3:
+            check_version()
+        case 4:
+            upgrade_pip_version()
+status = True
+
+while status == True:
+    command_choice(int(input(print(
+        "1. Install a Module.\n2. Uninstall a Module.\n3. Check Pip Version. \n4. Upgrade Pip Version\n\nWhat action would you like to take?"))))
+    continue_program= input(print("Would you like to take another action?\nY/N?"))
+    if continue_program.lower() == "n":
+        status = False
+        print("Program ended.\nThank you for using the Pip Tool.")
+
